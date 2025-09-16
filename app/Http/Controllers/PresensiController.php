8c0cc56a786289eb0117488a01bf8a0f09b0ad00
hexaviate <hexaviate@gmail.com>
2025-09-16 15:53:57 +0700

    revisi be #2
    menambahkan absensi bagi user yang tidak punya jadwal agar tetap bisa absensi

--- app/Http/Controllers/PresensiController.php
+++ app/Http/Controllers/PresensiController.php
@@ -38,9 +38,55 @@ class PresensiController extends Controller
         //* mengambil jadwal user dan guru
         $jadwal = Jadwal::where('user_id', $user->id)->where('instansi_id', $request->instansi_id)->where('hari', $hariIni)->where('tapel_id', $tapelAktif->id)->first();
 
+        // Cek apakah user sudah pernah presensi hari ini untuk instansi ini, dengan menggunakan created_at
+        $presensiHariIni = Presensi::where('user_id', $user)
+            ->where('instansi_id', $request->instansi_id)
+            ->whereDate('created_at', $now->toDateString())
+            ->first();
 
         if (!$jadwal) {
-            return response()->json(['status' => '❌ Jadwal tidak ditemukan'], 404);
+            if (!$presensiHariIni) {
+                if ($now->greaterThan(Carbon::createFromTime('06', '00', '00')) && $now->lessThan(Carbon::createFromTime('12', '00', '00'))) {
+                    Presensi::create([
+                        'instansi_id' => $request->instansi_id,
+                        "user_id" => $user->id, //diubah ketika testing final, nanti diisi user id
+                        "datang" => Carbon::now(),
+                        "status" => 'hadir',
+                        'tanggal' => Carbon::now()->toDateString(),
+                        'akurasi' => $request->akurasi,
+                        'userAgent' => $request->userAgent()
+                    ]);
+
+                    return response()->json([
+                        "status" => 'anda berhasil absensi' //status diganti ke return view blade
+                    ]);
+                } else {
+                    return response()->json([
+                        "status" => 'absen belum dibuka'
+                    ]); // status diganti ke return view blade
+                }
+            } else if (!$presensiHariIni->pulang) {
+                // dd($now);
+                if ($now->greaterThan(Carbon::createFromTime('12', '00', '00')) && $now->lessThan(Carbon::createFromTime('18', '00', '00'))) {
+
+                    $presensiHariIni->update([
+                        "pulang" => now()
+                    ]);
+                    return response()->json([
+                        "status" => 'anda berhasil absensi pulang'
+                    ]);
+                }
+            } else if ($presensiHariIni) {
+                if ($now->greaterThan(Carbon::createFromTime('06', '00', '00')) && $now->lessThan(Carbon::createFromTime('12', '00', '00'))) {
+                    return response()->json([
+                        "status" => 'anda telah absensi datang' //status diganti ke return view blade
+                    ]);
+                } else {
+                    return response()->json([
+                        'status' => 'anda'
+                    ]);
+                }
+            }
         }
 
         //mengambil jam datang dan pulang dari jadwal
@@ -48,15 +94,9 @@ class PresensiController extends Controller
         $jamPulang = Carbon::parse($jadwal->pulang);
 
 
-        // Cek apakah user sudah pernah presensi hari ini untuk instansi ini, dengan menggunakan created_at
-        $presensiHariIni = Presensi::where('user_id', $user)
-            ->where('instansi_id', $request->instansi_id)
-            ->whereDate('created_at', $now->toDateString())
-            ->first();
-
         // jika belum ada presensi
         if (!$presensiHariIni) {
-            if ($now->lessThan($jamPulang) && $now->greaterThan(Carbon::createFromTime('23', '00', '00'))) {
+            if ($now->lessThan($jamPulang) && $now->greaterThan(Carbon::createFromTime('6', '00', '00'))) {
                 Presensi::create([
                     'instansi_id' => $request->instansi_id,
                     "user_id" => $user->id, //diubah ketika testing final, nanti diisi user id
