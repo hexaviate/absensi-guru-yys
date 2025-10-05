21bba70dc7bd6872a7fab7b4f18d94b194635b1e
qmenggg <azizyaqy@gmail.com>
2025-10-05 09:16:37 +0700

    saya membuat dashboard yayasan fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,17 +12,19 @@ class DashboardController extends Controller
     public function operatorDashboard()
     {
         $user = auth()->user();
+        $instansi = $user->instansi->first();
         if (!$user) {
             return redirect()->route('login')->with('error', "anda belum login");
         }
 
-        if (!$user->role == 'operator_instansi') {
-            return redirect()->route('login')->with('error', "anda tidak punya akses");
-        }
+        // if (!$user->role == 'operator_instansi') {
+        //     return redirect()->route('login')->with('error', "anda tidak punya akses");
+        // }
 
-        $totalGuruInstansi = User::where('instansi_id', $user->instansi_id)->count();
-        $guruHadirHariIni = Presensi::where('instansi_id', $user->instansi_id)->whereDate('created_at', today())->latest()->get();
-        $totalGuruIzin = Izin::where('instansi_id', $user->instansi_id)->where('status', 'diterima')->whereDate('created_at', today())->count();
+        // $totalGuruInstansi = User::where('instansi_id', $instansi->id)->count();
+        $totalGuruInstansi = $instansi->user->count();
+        $guruHadirHariIni = Presensi::where('instansi_id', $instansi->id)->whereDate('created_at', today())->latest()->get();
+        $totalGuruIzin = Izin::where('instansi_id', $instansi->id)->where('status', 'diterima')->whereDate('created_at', today())->count();
 
         return view('dashboard.main', compact('totalGuruInstansi', 'guruHadirHariIni', 'totalGuruIzin', 'user'));
         // $guruBelumHadir = User::
