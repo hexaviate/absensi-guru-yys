7b5f37ca471f2f2d64a7e31329bd095e9ba38166
qmenggg <azizyaqy@gmail.com>
2025-10-19 08:13:03 +0700

    izin

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -61,7 +61,6 @@ class UsersController extends Controller
         $operatorRole = Role::whereIn('name', ['tenaga_pendidik', 'tenaga_kependidikan'])->get();
         $operatorInstansi = $user->instansi()->first();
 
-
         return view('user.tambah', compact('role', 'semuaUser', 'instansi', 'operatorRole', 'operatorInstansi'));
     }
 
